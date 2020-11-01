4e28bbaa83a5d1d0cef5cd38219063e535ae36fd
StartMeUpLille <contact@start-me-up.fr>
2020-11-01 14:54:09 +0100

    front-end ready

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,45 @@
-const $ = document;
-const query = (data) => {
-  return document.querySelector(data);
+const query = (str) => {
+  return document.querySelector(str);
 }
 
-$.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
+  // Target DOM elements
+  const modal = query("#modal-container");
   const closeIcon = query(".icon-times");
-  const form = query('#signupForm');
+  const form = query("#signupForm");
+  const signup = query("#signup");
+  const nameField = query("#name");
+  const emailField = query("#email");
+  const passwordField = query("#password");
+
+  // listeners
+
+  signup.addEventListener("click", () => {
+    modal.classList.toggle("hidden");
+  })
+
+  closeIcon.addEventListener("click", () => {
+    modal.classList.toggle("hidden");
+  })
+
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const data = {
+      name: nameField.value,
+      email: emailField.value,
+      password: passwordField.value,
+    }
+
+    console.log(data);
+
+    nameField.value = emailField.value = passwordField.value = "";
+
+    const response = await axios.post("https://tripadvisor-back.herokuapp.com/", data);
+    console.log(response);
 
 
-  const email = query("#email").value;
-  console.log(email);
+  })
+
 
 });
